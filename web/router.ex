f1aecabc1f5b563d77e4b1cdc1eabee7003a4d50
samwoodson <samuelwoodson@gmail.com>
2016-06-01 01:15:39 -0700

    guardian authentication

--- web/router.ex
+++ web/router.ex
@@ -9,18 +9,22 @@ defmodule PhoenixTrello.Router do
     plug :put_secure_browser_headers
   end
 
-  pipeline :api do
-    plug :accepts, ["json"]
-  end
-
   scope "/", PhoenixTrello do
     pipe_through :browser # Use the default browser stack
 
     get "*path", PageController, :index
   end
 
-  # Other scopes may use custom stacks.
-  # scope "/api", PhoenixTrello do
-  #   pipe_through :api
-  # end
+  pipeline :api do
+    plug :accepts, ["json"]
+  end
+
+  scope "/api", PhoenixTrello do
+    pipe_through :api
+
+    scope "/v1" do
+      post "/registrations", RegistrationsController, :create
+    end
+  end
+
 end
--- web/static/js/routes/index.js
+++ web/static/js/routes/index.js
@@ -1,6 +1,6 @@
 import { IndexRoute, Route } from 'react-router';
 import React from 'react';
-import MainLayout from '../layouts/main';
+import MainLayout from '../layout/main';
 import RegistrationsNew from '../views/registrations/new';
 
 export default (
