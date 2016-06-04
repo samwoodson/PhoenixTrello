7ee26e38a9b20a3be231a72ad3fa7da67cb615ff
samwoodson <samuelwoodson@gmail.com>
2016-06-03 19:28:38 -0700

    sockets implemented

--- web/static/js/reducers/session.js
+++ web/static/js/reducers/session.js
@@ -1,19 +1,25 @@
 import Constants from '../constants';
 
 const initialState = {
-  currentUser: null
+  currentUser: null,
+  socket: null,
+  channel: null,
+  error: null,
 };
 
 export default function reducer(state = initialState, action = {}) {
   switch (action.type) {
     case Constants.CURRENT_USER:
-      return { ...state, currentUser: action.currentUser };
+      return { ...state, currentUser: action.currentUser, error: null };
+
+    case Constants.USER_SIGNED_OUT:
+      return initialState;
+
+    case Constants.SOCKET_CONNECTED:
+      return { ...state, socket: action.socket, channel: action.channel };
 
     case Constants.SESSIONS_ERROR:
       return { ...state, error: action.error };
-      
-    case Constants.USER_SIGNED_OUT:
-      return initialState;
 
     default:
       return state;
